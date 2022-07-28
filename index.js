0845c881c76691a2e7bc7ee6f98f229788f8b9d7
Arpita <marpita721@gmail.com>
2022-07-29 01:59:10 +0530

    first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import postRoutes from './routes/posts.js';
+import postRoutes from './routes/posts.js'; 
 
 const app = express();
 
@@ -10,7 +10,6 @@ app.use('/posts',postRoutes);
 app.use(bodyParser.json({'limit':"30mb",extended:true}));
 app.use(bodyParser.urlencoded({'limit':"30mb",extended:true}));
 app.use(cors());
-
 
 const CONNECTION_URL = 'mongodb+srv://mern-website:Arpita2019@cluster0.wjucraz.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
